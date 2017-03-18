5c51764dda9467fe1095c494591c3fad8e2e76aa
Stefan Mielke <stefanmielke@fastmail.com>
2017-03-18 14:24:30 -0300

    Adding Compression methods.

---
+++ src/SupTree.Test/CompressionTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SupTree.Common;
+
+namespace SupTree.Test
+{
+    [TestFixture]
+    internal class CompressionTest
+    {
+        [Test]
+        public void CanCompressAndUncompressUsingGZip()
+        {
+            var largeString = LargeString.GetLargeString;
+
+            var bytes = Compression.GZip(largeString);
+            var newLargeString = Compression.UnGZip(bytes);
+
+            Assert.That(newLargeString, Is.EqualTo(largeString));
+        }
+    }
+}
---
+++ src/SupTree/SupTree.Common/Compression.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace SupTree.Common
+{
+    public static class Compression
+    {
+        public static byte[] GZip(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public static string UnGZip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    CopyTo(gs, mso);
+                }
+
+                return Encoding.UTF8.GetString(mso.ToArray());
+            }
+        }
+
+        private static void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+    }
+}
