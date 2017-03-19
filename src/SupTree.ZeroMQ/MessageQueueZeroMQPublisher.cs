5ef53fbefa0898ba7df8ebc9f355456dde64b090
Stefan Mielke <stefanmielke@fastmail.com>
2017-03-19 18:13:31 -0300

    ZeroMQ Pub/Sub implementation.

---
+++ src/SupTree.ZeroMQ/MessageQueueZeroMQPublisher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using SupTree.Common;
+using ZeroMQ;
+
+namespace SupTree.ZeroMQ
+{
+    public class MessageQueueZeroMQPublisher : IMessageSender
+    {
+        private readonly string _endpoint;
+
+        private static readonly object Blocker = new object();
+
+        public MessageQueueZeroMQPublisher(string endpoint)
+        {
+            _endpoint = endpoint;
+        }
+
+        public void Send(Message message)
+        {
+            var messageContent = JsonConvert.SerializeObject(message);
+            if (messageContent == null)
+                throw new ApplicationException("Could not serialize message");
+
+            var messageGziped = Compression.GZip(messageContent, Encoding.UTF8);
+
+            lock (Blocker)
+            {
+                using (var context = new ZContext())
+                {
+                    using (var requester = new ZSocket(context, ZSocketType.PUSH))
+                    {
+                        requester.Bind(_endpoint);
+
+                        using (var zmessage = new ZMessage())
+                        {
+                            zmessage.Add(new ZFrame(message.Tag));
+                            zmessage.Add(new ZFrame(messageGziped));
+
+                            requester.Send(zmessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/SupTree.ZeroMQ/MessageQueueZeroMQSubscriber.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using SupTree.Common;
+using ZeroMQ;
+
+namespace SupTree.ZeroMQ
+{
+    public class MessageQueueZeroMQSubscriber : IMessageReceiver
+    {
+        private readonly string _endpoint;
+        private readonly ZSocket _receiver;
+        private readonly ZContext _context;
+
+        public MessageQueueZeroMQSubscriber(string endpoint, string filter)
+        {
+            _endpoint = endpoint;
+            _context = new ZContext();
+            _receiver = new ZSocket(_context, ZSocketType.SUB);
+            _receiver.Connect(endpoint);
+            _receiver.Subscribe(filter);
+        }
+
+        public Message Receive()
+        {
+            using (var request = _receiver.ReceiveFrame())
+            {
+                var bytes = request.Read();
+
+                var unzipedContent = Compression.UnGZip(bytes, Encoding.UTF8);
+                return JsonConvert.DeserializeObject<Message>(unzipedContent);
+            }
+        }
+
+        public void Dispose()
+        {
+            _receiver.Disconnect(_endpoint);
+            _receiver.Dispose();
+            _context.Dispose();
+        }
+    }
+}
